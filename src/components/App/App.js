01f87de6e5250243bac5a2381ba1da83a0ab4ea1
kranz <francescpellegrini@gmail.com>
2018-01-28 16:36:19 +0100

    added i18next

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-// import { AppComp, AppHeader, AppLogo, AppTitle, AppIntro } from './styles.css';
-// import './styles.css';
 import styles from './styles.css'
+import { translate, Trans } from 'react-i18next';
 
 class App extends Component {
   render() {
+    const { t, i18n } = this.props;
+
+    const changeLanguage = (lng) => {
+      i18n.changeLanguage(lng);
+    }
     return (
       <div className={styles.AppComp}>
         <header className={styles.AppHeader}>
           <img src={logo} className={styles.AppLogo} alt="logo" />
-          <h1 className={styles.AppTitle}>Welcome to React</h1>
+          <h2 className={styles.AppTitle}>{t('title')}</h2>
+          <button onClick={() => changeLanguage('it')}>it</button>
+          <button onClick={() => changeLanguage('en')}>en</button>
+          <button onClick={() => changeLanguage('de')}>de</button>
         </header>
         <p className={styles.AppIntro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey="description.part1">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
-        <p className={styles.pippo}>ZIO PAPERONE</p>
+        <p className={styles.pippo}>{t('description.part2')}</p>
       </div>
     );
   }
 }
 
-export default App;
+export default translate('translations')(App);
